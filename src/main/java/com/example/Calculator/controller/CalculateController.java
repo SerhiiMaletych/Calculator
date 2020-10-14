242fbc42da26b7e98a26e060ea907051d7c48d0a
SerhiiMaletych <maitrewop@gmail.com>
2020-10-14 21:46:31 +0300

    #10 -  add adding controller and "result" field in template

--- src/main/java/com/example/Calculator/controller/CalculateController.java
+++ src/main/java/com/example/Calculator/controller/CalculateController.java
@@ -4,6 +4,8 @@ import com.example.Calculator.model.Operation;
 import com.example.Calculator.service.CalculateService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,6 +21,12 @@ public class CalculateController {
     @RequestMapping("/")
     public String getCalculatorPage(Model model){
         model.addAttribute("operation",operation);
+        return "main-page";
+    }
+
+    @PostMapping(value="/", params = "add")
+    public String add(@ModelAttribute("operation") Operation operation, Model model) {
+        model.addAttribute("result", service.add(operation));
         return "main-page";
     }
 }
