5d091c056c51eb3d068dbe555caecbeac8e26516
SerhiiMaletych <maitrewop@gmail.com>
2020-11-18 14:09:21 +0200

    #27 - add test for prime number

---
+++ src/test/java/com/example/Calculator/PrimeTests.java
@@ -0,0 +1,35 @@
+package com.example.Calculator;
+
+
+import com.example.Calculator.model.Operation;
+import com.example.Calculator.service.CalculateService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class PrimeTests {
+
+    @Autowired
+    CalculateService calculateService;
+
+    @Test
+    public void primeTest () {
+        Operation op1 = new Operation(4);
+        Operation op2 = new Operation(29);
+        Operation op3 = new Operation(83);
+
+        String result1 = calculateService.prime(op1);
+        String result2 = calculateService.prime(op2);
+        String result3 = calculateService.prime(op3);
+
+
+
+        assertEquals("Not a prime number", result1);
+        assertEquals("Prime number", result2);
+        assertEquals("Prime number", result3);
+    }
+}
